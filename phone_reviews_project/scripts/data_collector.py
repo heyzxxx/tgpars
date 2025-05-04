788615d2fc9641646ea6d4ed5971ceb07cdde0c7
heyzxxx <artemlilka2@gmail.com>
2025-05-04 11:09:49 +0300

    Update data_collector.py

--- phone_reviews_project/scripts/data_collector.py
+++ phone_reviews_project/scripts/data_collector.py
@@ -5,7 +5,7 @@ import os
 import nest_asyncio
 nest_asyncio.apply()
 
-# Вставь свои данные
+
 api_id = 27691773
 api_hash = '518514bab6ca37d91c944280dd2d60fa'
 client = TelegramClient('meduza_session', api_id, api_hash)
@@ -26,15 +26,15 @@ async def fetch_messages():
 
     df = pd.DataFrame(messages)
 
-    # Определяем корень проекта и путь к data/raw
+
     project_root = os.path.abspath(os.path.join(os.getcwd(), ".."))
     raw_data_dir = os.path.join(project_root, "data", "raw")
     os.makedirs(raw_data_dir, exist_ok=True)
 
-    # Сохраняем
+  
     output_path = os.path.join(raw_data_dir, "meduzalive.csv")
     df.to_csv(output_path, index=False)
     print(f"✅ Сохранено {len(df)} сообщений в:\n{output_path}")
 
-# Запуск
+
 await fetch_messages()
